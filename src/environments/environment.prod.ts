bf6fe27ffc7225ab2cef90708d01e3d7d11352ff
Sagar <sagark.kale@hotmail.com>
2019-03-01 23:49:48 +0530

    added firebase hostin tools and deployed app on firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCHWn_kloyRs5PBIaLN82e1liAHSukToAA",
+    authDomain: "dark-hall-126111.firebaseapp.com",
+    databaseURL: "https://dark-hall-126111.firebaseio.com",
+    projectId: "dark-hall-126111",
+    storageBucket: "dark-hall-126111.appspot.com",
+    messagingSenderId: "461206088003"
+
+  }
 };
